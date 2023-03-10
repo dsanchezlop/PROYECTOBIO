f660d8b9792b7e8f2cef10ec011338fd3089d0cf
dsanchezlop <dani.sanchez.lopez0@gmail.com>
2023-03-10 17:12:40 +0100

    Login and Register pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import database
 
 app = Flask(__name__)
@@ -7,13 +7,21 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-# @app.route('/acerca-de')
-# def acerca_de():
-#     return render_template('acerca_de.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+    
+@app.route('/redirect-to-login')
+def redirect_to_login():
+    return redirect(url_for('login'))
 
-# @app.route('/contacto')
-# def contacto():
-#     return render_template('contacto.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+    
+@app.route('/redirect-to-register')
+def redirect_to_register():
+    return redirect(url_for('register'))
 
 if __name__ == '__main__':
     
--- copypaste.py
+++ copypaste.py
@@ -28,17 +28,72 @@ if __name__ == '__main__':
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 
-    <h1>Bienvenido a mi sitio web</h1>
-    <p>Esta es la página de inicio</p>
-</body>
-</html>
+
+
+.container {
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+    text-align: center;
+    font-size: 2em;
+    margin-bottom: 20px;
+    
+}
+
+.form-group {
+    margin-bottom: 20px;
+    max-width: 500px;
+}
+
+label {
+    display: block;
+    margin-bottom: 5px;
+    font-size: 1.2em;
+    font-weight: bold;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+    width: 100%;
+    padding: 10px;
+    font-size: 1.2em;
+    border: 2px solid #ccc;
+    border-radius: 5px;
+    box-sizing: border-box;
+}
+
+input[type="submit"] {
+    background-color: #007bff;
+    color: #fff;
+    padding: 10px;
+    font-size: 1.2em;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+}
+
+input[type="submit"]:hover {
+    background-color: #0062cc;
+}
+
+.error-message {
+    color: red;
+    font-size: 1.1em;
+    margin-top: 10px;
+}
+
+/* Media queries */
+@media screen and (max-width: 600px) {
+    .container {
+        max-width: 100%;
+        padding: 10px;
+    }
+}
