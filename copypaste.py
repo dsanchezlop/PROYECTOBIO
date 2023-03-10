a9c751033423cc4598f10ead020a10fad15eac78
dsanchezlop <dani.sanchez.lopez0@gmail.com>
2023-03-10 16:38:32 +0100

    HABEMUS PÁGINA
    Página ya funcional

--- copypaste.py
+++ copypaste.py
@@ -23,3 +23,22 @@ def register_post():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Bienvenido a mi sitio web</h1>
+    <p>Esta es la página de inicio</p>
+</body>
+</html>
--- database.py
+++ database.py
@@ -12,13 +12,13 @@ app.config['MYSQL_DB'] = 'proyecto-bio'
 
 mysql = MySQL(app)
 
-with mysql.connection.cursor() as cursor:
-    cursor.execute('CREATE TABLE countries (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) PRIMARY KEY, code VARCHAR(5))')
-    cursor.execute('CREATE TABLE fertilizers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) PRIMARY KEY')
-    cursor.execute('CREATE TABLE year (year DATE)')
-    cursor.execute('CREATE TABLE country_fertilizer (id INT AUTO_INCREMENT PRIMARY KEY, name_country VARCHAR(30), name_fertilizer VARCHAR(30)), kg_h FLOAT, year DATE, FOREIGN KEY (name_country) REFERENCES countries(name), FOREIGN KEY (name_fertilizer) REFERENCES fertilizers(name), FOREIGN KEY (year) REFERENCES year(year)')
-    cursor.execute('ALTER TABLE country_fertilizer ADD PRIMARY KEY (name_country, name_fertilizer, year);)')
-    mysql.connection.commit()
+# with mysql.connection.cursor() as cursor:
+#     cursor.execute('CREATE TABLE countries (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) PRIMARY KEY, code VARCHAR(5))')
+#     cursor.execute('CREATE TABLE fertilizers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) PRIMARY KEY')
+#     cursor.execute('CREATE TABLE year (year DATE)')
+#     cursor.execute('CREATE TABLE country_fertilizer (id INT AUTO_INCREMENT PRIMARY KEY, name_country VARCHAR(30), name_fertilizer VARCHAR(30)), kg_h FLOAT, year DATE, FOREIGN KEY (name_country) REFERENCES countries(name), FOREIGN KEY (name_fertilizer) REFERENCES fertilizers(name), FOREIGN KEY (year) REFERENCES year(year)')
+#     cursor.execute('ALTER TABLE country_fertilizer ADD PRIMARY KEY (name_country, name_fertilizer, year);)')
+#     mysql.connection.commit()
 
 # import mysql.connector
 
