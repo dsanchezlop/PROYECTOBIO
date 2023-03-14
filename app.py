5cb545efa24ec3054b658f2d3b7da8015a30ff67
dsanchezlop <dani.sanchez.lopez0@gmail.com>
2023-03-14 17:38:33 +0100

    USER MANAGEMENT ALMOST COMPLETE
    WE NEED THE VISITOR ROLE

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, url_for
-import mysql.connector
-import hashlib
+import database
+
+
 
 app = Flask(__name__)
 
@@ -21,94 +22,54 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        name = request.form['name']
-        surname = request.form['surname']
-        password = request.form['password']
-        confirm_password = request.form['confirm-password']
-
-        # hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        if password == confirm_password:
-            conn = mysql.connector.connect(**db_config)
-            cursor = conn.cursor()
-            try:
-                # query = "SELECT username FROM users WHERE username = %s"
-                # cursor.execute(query, (username,))
-                # result = cursor.fetchone()
-                # cursor.close()
-                # conn.close()
-                # if result is not None:
-                #     return render_template('register.html', error='Invalid username')
-                # else:
-                #     query2 = "SELECT email FROM users WHERE email = %s"
-                #     cursor.execute(query2, (email,))
-                #     conn.commit()
-                #     result2 = cursor.fetchone()
-                #     cursor.close()
-                #     conn.close()
-                #     if result2 is not None:
-                #         return render_template('register.html', error='Invalid email')
-                #     else:
-                        query3 = "INSERT INTO users (username, email, name, surname, passw, role) VALUES (%s, %s, %s, %s, %s, 2)"
-                        cursor.execute(
-                            query3, (username, email, name, surname, password))
-                        conn.commit()
-                        cursor.close()
-                        conn.close()
-                        session['username'] = username
-                        return redirect('/login')
-            except mysql.connector.Error as error:
-                print(f"Error while executing SQL query: {error}")
-                return render_template('register.html', error='Username or email not valid')
-        else:
-            return render_template('register.html', error='Passwords do not match')
+    if 'username' not in session: 
+        result = database.registration()
+        return result
     else:
-        return render_template('register.html')
+        return redirect('/')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    print(request.method)
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
-        query = "SELECT username, passw FROM users WHERE username = %s"
-        cursor.execute(query, (username,))
-        result = cursor.fetchone()
-        cursor.close()
-        conn.close()
-
-        if result is not None and result[1] == password:
-            session['username'] = username
-            return redirect('/')
-        else:
-            return render_template('login.html', error='Invalid username or password')
+    if 'username' not in session:
+        result = database.log_in()
+        return result
     else:
-        return render_template('login.html')
+        return redirect('/')
 
+@app.route('/logout', methods=['GET'])
+def logout():
+    if 'username' in session:
+        result = database.log_out()
+        return result
+    else:
+        return redirect('/')
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if 'username' not in session:
+        return redirect('/')
+    else:
+        return render_template('profile.html')
+
+@app.route('/database', methods=['GET', 'POST'])
+def data_base():
+    return render_template('database.html')
+
+@app.route('/maps', methods=['GET', 'POST'])
+def maps():
+    return render_template('maps.html')
 
 @app.route('/dashboard')
 def dashboard():
-    if 'email' in session:
+    if 'username' in session:
         return render_template('dashboard.html')
     else:
         return redirect('/login')
 
-
-@app.route('/redirect-to-login')
-def redirect_to_login():
-    return redirect(url_for('login'))
-
-
-@app.route('/redirect-to-register')
-def redirect_to_register():
-    return redirect(url_for('register'))
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect('/')
 
 if __name__ == '__main__':
 
--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for
+
 import mysql.connector
 import hashlib
 
@@ -12,48 +13,71 @@ db_config = {
     'database': 'proyecto-bio'
 }
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         password = request.form['password']
-#         hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
-#         conn = mysql.connector.connect(**db_config)
-#         cursor = conn.cursor()
-#         query = "INSERT INTO users (email, password) VALUES (%s, %s)"
-#         cursor.execute(query, (email, hashed_password))
-#         conn.commit()
-#         cursor.close()
-#         conn.close()
 
-#         session['email'] = email
-#         return redirect('/dashboard')
-#     else:
-#         return render_template('register.html')
+def registration():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        name = request.form['name']
+        surname = request.form['surname']
+        password = request.form['password']
+        confirm_password = request.form['confirm-password']
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+        # hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        if password == confirm_password:
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor()
+            try:
+                query3 = "INSERT INTO users (username, email, name, surname, passw, role) VALUES (%s, %s, %s, %s, %s, 2)"
+                cursor.execute(
+                    query3, (username, email, name, surname, password))
+                conn.commit()
+                cursor.close()
+                conn.close()
+                session['username'] = username
+                return redirect('/login')
+            except mysql.connector.Error as error:
+                print(f"Error while executing SQL query: {error}")
+                return render_template('register.html', error='Username or email not valid')
+        else:
+            return render_template('register.html', error='Passwords do not match')
+    else:
+        return render_template('register.html')
+    
+
+def log_in():
+    print(request.method)
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-
+        # hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
-        query = "SELECT username, password FROM users WHERE username = %s"
+        query = "SELECT username, passw, role FROM users WHERE username = %s"
         cursor.execute(query, (username,))
         result = cursor.fetchone()
         cursor.close()
         conn.close()
 
-        if result is not None and result[1] == hashed_password:
+        if result is not None and result[1] == password:
             session['username'] = username
-            return redirect('/dashboard')
+            session['logged_in'] = True
+            session['role'] = result[2]
+            print (result[2])
+            print (session['role'])
+            return redirect('/')
         else:
             return render_template('login.html', error='Invalid username or password')
     else:
         return render_template('login.html')
+
+
+def log_out():
+    session.pop('username', None)
+    session['logged_in'] = False
+    return redirect('/')
+    
 
 @app.route('/dashboard')
 def dashboard():
