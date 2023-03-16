93f17ebb0932da2c5e79534a93972304c0cb55ec
dsanchezlop <dani.sanchez.lopez0@gmail.com>
2023-03-16 20:44:55 +0100

    das

--- angular-app/src/app/classes/user.ts
+++ angular-app/src/app/classes/user.ts
@@ -15,6 +15,51 @@ export class User {
         this.#role = role;
     }
 
+    public get username() {
+        return this.#username;
+    }
 
+    public get name() {
+        return this.#name;
+    }
 
+    public get surname() {
+        return this.#surname;
+    }
+
+    public get password() {
+        return this.#password;
+    }
+
+    public get email() {
+        return this.#email;
+    }
+
+    public get role() {
+        return this.#role;
+    }
+
+    public set username(username){
+        this.#username = username;
+    }
+
+    public set name(name){
+        this.#name = name;
+    }
+
+    public set surname(surname){
+        this.#surname = surname;
+    }
+
+    public set password(password){
+        this.#password = password;
+    }
+
+    public set email(email){
+        this.#email = email;
+    }
+
+    public set role(role){
+        this.#role = role;
+    }
 }
